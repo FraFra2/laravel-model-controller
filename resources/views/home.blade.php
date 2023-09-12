c0863adcac5bab047e9391508458075fa5dcb6d4
FraFra2 <kekkino41@gmail.com>
2023-09-12 15:49:13 +0200

    ADD Credenziali env/clonato DB

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,12 @@ Homepage
 @endsection --}}
 
 @section('main-content')
+
 <h1>
     Homepage
 </h1>
 
-<div class="gatto"></div>
 
-<img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt="">
+
+{{-- <img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt=""> --}}
 @endsection
